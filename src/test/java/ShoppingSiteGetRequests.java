2586b51a02143b1eae4878f40b23aa8b34b5278d
Saghir Ayub <saghir.ayub@ten10.com>
2022-12-13 15:21:33 +0000

    added a test to see if a guest can view account information

--- src/test/java/ShoppingSiteGetRequests.java
+++ src/test/java/ShoppingSiteGetRequests.java
@@ -1,3 +1,7 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import org.junit.Assert;
 import org.junit.Test;
 
 import static io.restassured.RestAssured.when;
@@ -11,5 +15,12 @@ public class ShoppingSiteGetRequests {
                 get(BASE_URL).
         then().
                 statusCode(200);
+    }
+
+    @Test
+    public void viewMyAccountNotLoggedIn(){
+        String htmlContent = when().
+                get(BASE_URL+"?controller=my-account").htmlPath().getString("html.body");
+        Assert.assertTrue("Log in to your account not present",htmlContent.contains("Log in to your account"));
     }
 }
