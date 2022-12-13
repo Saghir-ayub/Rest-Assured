c99afbe3cdbda6024432c866cc755dd1f4fec0e3
Saghir Ayub <saghir.ayub@ten10.com>
2022-12-13 12:05:44 +0000

    rest assured project

---
+++ src/test/java/GetRequest.java
@@ -0,0 +1,11 @@
+import io.restassured.RestAssured.*;
+import io.restassured.matcher.RestAssuredMatchers.*;
+import org.hamcrest.Matchers.*;
+import org.junit.Test;
+
+public class GetRequest {
+    @Test
+    public void getRequest(){
+        
+    }
+}
