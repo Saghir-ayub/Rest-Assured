1ac0e9dd207be154a85a4e236e0ab73a4e9c81a6
Saghir Ayub <saghir.ayub@ten10.com>
2022-12-13 14:04:22 +0000

    added get homepagerequest

--- src/test/java/ShoppingSiteGetRequests.java
+++ src/test/java/ShoppingSiteGetRequests.java
@@ -5,5 +5,8 @@ import static org.hamcrest.CoreMatchers.containsString;
 
 public class ShoppingSiteGetRequests {
     private static final String BASE_URL = "http://3.11.77.136/";
-
+    @Test
+    public void getHomepage(){
+        
+    }
 }
