5566be59f8075eb18e117e6e3613098d50880ddb
Saghir Ayub <saghir.ayub@ten10.com>
2022-12-13 12:15:50 +0000

    getting something

--- src/test/java/GetRequest.java
+++ src/test/java/GetRequest.java
@@ -3,9 +3,11 @@ import io.restassured.matcher.RestAssuredMatchers.*;
 import org.hamcrest.Matchers.*;
 import org.junit.Test;
 
+import static io.restassured.RestAssured.when;
+
 public class GetRequest {
     @Test
     public void getRequest(){
-        
+        when().get("https://swapi.dev/api/people/1").then().equals(5);
     }
 }
