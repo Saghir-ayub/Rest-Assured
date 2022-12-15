78d0d21310a571e25cf8e71c6a7b2c535e8962fd
hannahst17 <hannah.stormonth@ten10.com>
2022-12-15 14:29:48 +0000

    Added test for checking invalid endpoint results in status code 404

---
+++ src/test/java/RequestTests/InvalidPageTest.java
@@ -0,0 +1,17 @@
+package RequestTests;
+
+import ServiceObjects.InvalidPage;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+public class InvalidPageTest {
+
+    InvalidPage invalidPage = new InvalidPage();
+    
+    @Test
+    @Tag("AUT-17")
+    public void checkInvalid() {
+        invalidPage.goToInvalidPage();
+        invalidPage.statusCodeOfPage(404);
+    }
+}
---
+++ src/test/java/ServiceObjects/InvalidPage.java
@@ -0,0 +1,20 @@
+package ServiceObjects;
+
+import org.junit.jupiter.api.Assertions;
+
+public class InvalidPage extends BasePage {
+
+    private static final String EXTENSION = "?controller=basket";
+
+    public InvalidPage() {
+        super();
+    }
+
+    public void goToInvalidPage() {
+        response = httpRequest.get(EXTENSION);
+    }
+
+    public void statusCodeOfPage(int statusCode) {
+        Assertions.assertEquals(statusCode, getStatusCodeOfPage());
+    }
+}
